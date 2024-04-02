0baeb75e1b8cf1616bc83f7f63e01bb4f89720b0
patrickarmani <arm22001@byui.edu>
2024-04-02 07:58:35 -0300

    validators

--- routes/countries.js
+++ routes/countries.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const countriesController = require('../controllers/countries');
+const validation = require('../middleware/validate');
 
 router.get('/', countriesController.getAll);
 
 router.get('/:id', countriesController.getSingle);
 
-router.post('/', countriesController.createCountry);
+router.post('/', validation.saveCountry, countriesController.createCountry);
 
-router.put('/:id', countriesController.updateCountry);
+router.put('/:id', validation.saveCountry, countriesController.updateCountry);
 
 router.delete('/:id', countriesController.deleteCountry);
 
