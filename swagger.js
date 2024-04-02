a50a9cbe35046f59af5a566b540e9ce1eee7e1d6
patrickarmani <arm22001@byui.edu>
2024-04-02 09:39:58 -0300

    update swagger.json

--- swagger.js
+++ swagger.js
@@ -11,5 +11,5 @@ const doc = {
 const outputFile = 'swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-//this will generate swagger.json
+// This will generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
